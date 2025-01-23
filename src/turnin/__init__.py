e5d414defbc675909309ec96f70f045902f71ae9
Todd O'Bryan <toddobryan@gmail.com>
2025-01-23 10:14:22 -0500

    Remove turnin stuff

--- src/turnin/__init__.py
+++ src/turnin/__init__.py
@@ -2,20 +2,20 @@ from argparse import ArgumentParser
 from datetime import datetime
 from pathlib import Path
 
-from .collect import collect
+#from .collect import collect
 
-turnin_parser = ArgumentParser(prog="turnin", description="handle turnin procedures")
-turnin_parser.add_argument("command", choices=["collect", "move_to_turnin"])
-turnin_parser.add_argument("-e", "--environment", choices=["prod", "test"], default="test")
-
-subparsers = turnin_parser.add_subparsers(dest="command")
-collect_parser = subparsers.add_parser("collect", description="collect student work")
-collect_parser.add_argument("period")
-collect_parser.add_argument("assignment_file")
-collect_parser.add_argument("directory")
-
-move_to_turnin_parser = subparsers.add_parser("move_to_turnin", description="move student work into turnin folder")
-move_to_turnin_parser.add_argument("from_directory")
+# turnin_parser = ArgumentParser(prog="turnin", description="handle turnin procedures")
+# turnin_parser.add_argument("command", choices=["collect", "move_to_turnin"])
+# turnin_parser.add_argument("-e", "--environment", choices=["prod", "test"], default="test")
+#
+# subparsers = turnin_parser.add_subparsers(dest="command")
+# collect_parser = subparsers.add_parser("collect", description="collect student work")
+# collect_parser.add_argument("period")
+# collect_parser.add_argument("assignment_file")
+# collect_parser.add_argument("directory")
+#
+# move_to_turnin_parser = subparsers.add_parser("move_to_turnin", description="move student work into turnin folder")
+# move_to_turnin_parser.add_argument("from_directory")
 
 
 ASSIGNMENTS_FOLDER = Path("/") / "home" / "toddobryan" / "assignments"
@@ -36,10 +36,10 @@ def datetime_tag() -> str:
     """Returns current datetime in the form YYYY-MM-DD-HH-MM"""
     return datetime.now().strftime("%Y-%m-%d-%H-%M")
 
-if __name__ == "__main__":
-    turnin_args = turnin_parser.parse_args()
-    missing = collect(periods, assignments)
-    for login, assignments in missing.items():
-        print(f"{login}: {", ".join(assignments)}")
-    print("\n  Don't forget to chown assignments.")
+# if __name__ == "__main__":
+#     turnin_args = turnin_parser.parse_args()
+#     missing = collect(periods, assignments)
+#     for login, assignments in missing.items():
+#         print(f"{login}: {", ".join(assignments)}")
+#     print("\n  Don't forget to chown assignments.")
 
